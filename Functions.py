38fbeff5977d25b780ac95e7604da9b52b11b9e2
Jiani Li <103307824+Jiani-Li@users.noreply.github.com>
2022-11-11 14:52:38 +0100

    Update Functions.py

--- Functions.py
+++ Functions.py
@@ -1,10 +1,7 @@
-from classes import Song
-
-# Function 1: read_shazam_to_get_raw_lines
-# read_shazam_to_get_raw_lines is a function which read the content of a file
+# This is a function which read the content of a file
 # and transfer it into a list of each line in the file
-def read_shazam_to_get_raw_lines(file1):
-    with open(file=file1, mode='r', encoding='utf-8') as f1:
+def read_file_to_get_raw_lines(filename):
+    with open(file=filename, mode='r', encoding='utf-8') as f1:
         # Create a list named raw_lines to store the lines without space or line break surrounding:
         raw_lines = []
         for line in f1.readlines():
@@ -16,8 +13,7 @@ def read_shazam_to_get_raw_lines(file1):
     return raw_lines
 
 
-# Function 2: get_indices
-# get_indices is a function that create a list called index_list to store all the index numbers in raw_data:
+# This is a function that create a list called index_list to store all the index numbers in raw_data:
 def get_indices(raw_lines):
     index_list = []
     for index_num in raw_lines:
@@ -27,8 +23,7 @@ def get_indices(raw_lines):
     return index_list
 
 
-# Function 3: get_songs_raw_data
-# From the raw_lines and index_list to get the music list
+# This is the function that generate a list of all index, song names and artists from the indices and raw_lines
 def get_songs_raw_data(indices, raw_lines):
     # Get the first index and last index value:
     first_index_value = indices[0]
@@ -39,19 +34,28 @@ def get_songs_raw_data(indices, raw_lines):
     last_song_index = raw_lines.index(last_index_value)
 
     # Generate the music list based on the first_song_index and last_song_index:
-    music_list = raw_lines[first_song_index:last_song_index + 3]
-    return music_list
+    song_raw_list = raw_lines[first_song_index:last_song_index + 3]
+    return song_raw_list
 
 
-# Function 4: get_song_tuple_list
-# Spilt the music form music_list to a list of tuple
+# This is the function to get a list of tuple of song from the song_raw_list
 # In this case, we spilt the list in every step 3:
-def get_song_tuple_list(music_list):
-    tuple_list = [tuple(music_list[i:i+3]) for i in range(0, len(music_list), 3)]
-    return tuple_list
+def get_song_tuple_list(song_raw_list):
+    song_tuple_list = [tuple(song_raw_list[i:i+3]) for i in range(0, len(song_raw_list), 3)]
+    return song_tuple_list
 
 
-# Function 5: get_song_objects
-# Create the function to pass all the songs into the object of Class Song:
-def get_song_objects(tuple_list):
-    return [Song(song[0], song[1], song[2]) for song in tuple_list]
+from wordcloud import WordCloud
+
+# This is the function to generate the image of word cloud from the given text
+def get_wordcloud(text):
+
+    wordcloud = WordCloud(
+        background_color='white',
+        width=800,
+        height=600,
+        margin=2,
+        stopwords={'feat', 'remix', 'Radio', 'Edit', 'The', 'I', 'You'}
+    ).generate(text)
+
+    return wordcloud
