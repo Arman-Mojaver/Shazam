6696c278173cc5387e26224cceaa0f93f4a29cef
Jiani Li <103307824+Jiani-Li@users.noreply.github.com>
2022-11-30 15:08:38 +0100

    create class :DataHandler and change code in main (#6)
    * create class :DataHandler and change code in main
    
    * comment fixes, refactor get_indices() method with list comprehension

--- classes.py
+++ classes.py
@@ -33,3 +33,79 @@ class Songs:
 
     def get_song_titles(self):
         return [song.song_title for song in self.song_objects]
+
+    def print(self):
+        for song in self.song_objects:
+            print(song)
+
+# Create a class named DataHandler that handle the data from the file and remove duplicates
+class DataHandler:
+    def __init__(self, file):
+        self.file = file
+        self.raw_lines = self.read_file_to_get_raw_lines()
+        self.indices = self.get_indices()
+        self.song_raw_list = self.get_songs_raw_data()
+        self.song_tuple_list = self.get_song_tuple_list()
+        self.unique_songs = self.remove_duplicates()
+        self.reindexed_songs = self.reindex()
+
+    # This is a function which read a file and transfer it into a list of every line in the file
+    def read_file_to_get_raw_lines(self):
+        with open(file=self.file, mode='r', encoding='utf-8') as f:
+            # Create a list named raw_lines to store the lines without space or line break surrounding:
+            raw_lines = []
+            for line in f.readlines():
+                # Remove the surrounding space and line break of each line in f
+                strip_string = line.strip()
+                if strip_string:
+                    raw_lines.append(strip_string)
+
+        return raw_lines
+
+    # From the function: read_file_to_get_raw_lines, we get the list of every line in the file.
+    # We need to remove the unnecessary lines, and only keep the index, song titles and artists.
+    # In order to do that, firstly we need to get all the song index numbers and put them into a list.
+    # This is a function that create a list called index_list to store all the index numbers in raw_data:
+    def get_indices(self):
+        return [index_num for index_num in self.raw_lines if index_num.isdigit()]
+
+    # From the function:get_indices, we get the list of index numbers from the file.
+    # Based on this, we can create a function named get_songs_raw_data to
+    # generate a list of all index, song names and artists from the indices and raw_lines.
+    def get_songs_raw_data(self):
+        # Get the first index and last index value:
+        first_index_value = self.indices[0]
+        last_index_value = self.indices[-1]
+
+        # Find the location of the first_index_value and last_index_value in the raw_lines
+        first_song_index = self.raw_lines.index(first_index_value)
+        last_song_index = self.raw_lines.index(last_index_value)
+
+        # Generate the music list based on the first_song_index and last_song_index:
+        song_raw_list = self.raw_lines[first_song_index:last_song_index + 3]
+
+        return song_raw_list
+
+    # From the method get_songs_raw_data(), we get a list of index number, song title and artist.
+    # Based on this, we can create a method named get_song_tuple_list() to put the index, song title and artist
+    # of every song into a tuple and then put all the tuples into a list.
+    def get_song_tuple_list(self):
+        return [tuple(self.song_raw_list[i:i + 3]) for i in range(0, len(self.song_raw_list), 3)]
+
+    # From the method get_song_tuple_list(), we get the list of tuple of song.
+    # But it may have duplicates in this list, so we need to remove duplicates.
+    # method remove_duplicates() is the function to get the unique tuple of the song title and artist
+    def remove_duplicates(self):
+        return list(set([(index_title_artist[1], index_title_artist[2]) for index_title_artist in self.song_tuple_list]))
+
+    # From the method remove_duplicates(), we get the list of unique tuple of the song title and artist
+    # Based on this, we create a function named reindex to add the index into every tuple
+    # In this method, we also sort the songs by artist name.
+    def reindex(self):
+        sorted_songs = sorted(self.unique_songs, key=lambda unique_songs: unique_songs[1])
+        return [(index, song[0], song[1]) for index, song in enumerate(sorted_songs, 1)]
+
+    def print(self):
+        for song in self.reindexed_songs:
+            print(song)
+
--- main.py
+++ main.py
@@ -1,37 +1,22 @@
-from functions import (
-    read_file_to_get_raw_lines,
-    get_indices,
-    get_songs_raw_data,
-    get_song_tuple_list,
-    remove_duplicates,
-    reindex,
-    get_wordcloud
-)
-from classes import Songs
-import matplotlib.pyplot as plt
+from classes import Songs, DataHandler
+# import matplotlib.pyplot as plt
 
 # Open the file which is named shazam.txt and read the content of this file
 my_file = 'shazam.txt'
-# We convert the content from the shazam.txt into a list of tuple of a song
-# including index, song title and artist step by step.
-raw_lines = read_file_to_get_raw_lines(my_file)
-indices = get_indices(raw_lines)
-song_raw_list = get_songs_raw_data(indices, raw_lines)
-song_tuple_list = get_song_tuple_list(song_raw_list)
-unique_songs = remove_duplicates(song_tuple_list)
-reindexed_songs = reindex(unique_songs)
+data_handler = DataHandler(my_file)
+# Print all the reindexed songs in the class of DataHandler
+data_handler.print()
 
 # songs is the list of tuples of songs from the original file
-songs = Songs(song_data=reindexed_songs)
-# print all the song objects
-for song in songs.song_objects:
-    print(song)
+songs = Songs(song_data=data_handler.reindexed_songs)
+# Print all the reindexed songs in the class of Songs
+songs.print()
 
 # We create the word cloud of the artists
-text = ' '.join(songs.artists)
-wordcloud_1 = get_wordcloud(text)
-plt.imshow(wordcloud_1)
-plt.axis('off')
-plt.show()
+# text = ' '.join(songs.artists)
+# wordcloud_1 = get_wordcloud(text)
+# plt.imshow(wordcloud_1)
+# plt.axis('off')
+# plt.show()
 
-wordcloud_1.to_file('shazam_artist_wordcloud.png')
+# wordcloud_1.to_file('shazam_artist_wordcloud.png')
