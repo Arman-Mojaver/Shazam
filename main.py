7f8fbd419f9d999657c870ba58a3637f8e029b51
Jiani Li <lijianiwinwin@126.com>
2022-12-01 18:01:03 +0100

    create a json file named 'old_songs.json' with a list of the songs that Please enter the commit message for your changes. Lines starting

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 from classes import Songs, DataHandler
+import json
 # import matplotlib.pyplot as plt
 
 # Open the file which is named shazam.txt and read the content of this file
 my_file = 'shazam.txt'
 data_handler = DataHandler(my_file)
 # Print all the reindexed songs in the class of DataHandler
-data_handler.print()
+song_list = data_handler.reindexed_songs
+json_songs = json.dumps(song_list, ensure_ascii=False, indent=2)
+with open(file='old_songs.json', mode='w', encoding='utf-8') as f:
+    f.write(json_songs)
+    f.close()
 
 # songs is the list of tuples of songs from the original file
 songs = Songs(song_data=data_handler.reindexed_songs)
 # Print all the reindexed songs in the class of Songs
-songs.print()
+# songs.print()
+
 
 # We create the word cloud of the artists
 # text = ' '.join(songs.artists)
